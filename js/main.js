eb60f089f39e0acae7494b1370c1cc0d2aacf957
Anton Khrystsinich <antonkhrystsinich@gmail.com>
2018-01-19 23:00:20 +0300

    Add files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+    $('.contact-us__up--btn').on('click', function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+function animate(elem) {
+    var effect = elem.data('effect');
+    elem.addClass('animated ' + effect).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend', function () {
+        elem.removeClass('animated ' + effect);
+    });
+}
+
+$(document).ready(function () {
+    $('.footer__social-link').mouseenter(function () {
+        animate($(this));
+    })
+});
+
+// Owl Carousel
+$(document).ready(function() {
+    $("#owl-slider").owlCarousel({
+
+        navigation : true,
+        slideSpeed : 400,
+        paginationSpeed : 400,
+        singleItem : true
+
+    });
+});
